64bae083c241f8a0ebcd1594da98871cd2f4c9f2
yeisonconsuegra <81399945+yeisonconsuegra@users.noreply.github.com>
2021-04-01 15:09:45 -0500

    Add files via upload

---
+++ Proyecto 1/index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="practica.css">
+	<title>Color Cacao</title>
+</head>
+<body>
+	<div class="container-nav">
+		<header>
+				<img src="logo_cacao.svg" class="img1">
+			<ul>
+				<li><a href="#" class="a-header"><span class="a-1">INICIO</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-2">SOMOS</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-3">PRODUCTOS</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-4">CHOCOLATES <br> CORPORATIVOS</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-5">DISTRIBUIDORES</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-6">TIENDA</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-7">CONTACTO</span></a></li>
+				<li><a href="#" class="a-header"><span class="a-8">BLOG</span></a></li>
+			</ul>
+		</header>
+	</div>
+	<main>
+		<img src="cacao.jpg" class="img-cacao">
+		<div class="_h1">
+			<h1>BIENVENIDOS A COLOR CACAO</h1>
+			<p class="h1-p">la expresión contemporánea del chocolate de tradición</p>
+			<div class="a-buttom">
+				<a href="#" class="_h1-a-1">Conócenos</a>
+				<a href="#" class="_h1-a-2">Contacto</a>
+			</div>
+		</div>
+		<div class="produc-i">
+			<div class="div-produc">
+				<img src="produc-1.jpg">
+				<div class="pink">
+					<p class="p-pro">10 BARRAS DE CHOCOLATE DE ORIGEN MARACUYÁ 65%- 60G</p><br>
+				    <p class="press">$78,000</p>
+				</div>
+			</div>
+			<div class="div-produc">
+				<img src="produc-2.jpg">
+				<div class="pink">
+					<p class="p-pro">10 BARRAS DE CHOCOLATE DE ORIGEN COCO 55%- 60G</p><br>
+				    <p class="press">$118,000</p>
+				</div>
+			</div>
+			<div class="div-produc">
+				<img src="produc-3.jpg">
+				<div class="pink">
+					<p class="p-pro">CHOCOLATERAPIA EN CASA</p><br>
+				    <p class="press">$50,000</p>
+				</div>
+			</div>
+		</div>
+		<div class="_h3-cho">
+			<h2 class="h3-h3">Bienvenidos a la <span class="span-h3">máxima expresión</span> del chocolate</h2>
+			<p class="p-h3">Color Cacao es una empresa productora de chocolates finos artesanales, fundada en 2011 en Medellín, Colombia. <b>Transformamos el Cacao de Origen de los cultivadores campesinos en auténticos e innovadores regalos de chocolate que puedes</b> comprar <a href="#" class="a-h3">aquí</a>. Contamos con una red de distribuidores que puedes consultar <a href="#" class="a-h3">aquí</a>.</p>
+			<p class="p-h3"><b>Diseñamos, producimos y empacamos chocolates corporativos</b> personalizados que puedes ver <a href="#" class="a-h3">aquí</a>.</p>
+			<div >
+				<a href="#" class="link-c">Chocolates</a>
+				<a href="#" class="link-p">Productos</a>
+			</div>
+		</div>
+		<br><br>
+		<div class="container-morado">
+			<div class="h3-morado">
+				<h3 class="h3-title">Nos apaciona <span class="m-s">lo que hacemos</span></h3>
+				<p class="h3-title-p"><b>Amamos el chocolate</b> y con la mayor dedicación y alegría elaboramos múltiples <b>productos con cacaos de origen para obsequiar y consentir tus sentidos.</b> Aplicamos las últimas <b>tendencias en chocolatería fina mundial</b> tanto en sabor como en diseño</p>
+			</div>
+			<div class="container-img">
+				<div class="img-conte">
+					<img src="morado-1.jpg" class="img-morado-m"><br>
+					<div class="pes-morado">
+						<p class="p-morado">
+							Consiente tu paladar y regala obsequios increíbles
+						</p><br>
+						<a href="#" class="a-morado">Conoce nuestros chocolates</a>
+					</div>
+				</div>
+				<div class="img-conte">
+					<img src="morado-2.jpg" class="img-morado-m"><br>
+					<div class="pes-morado">
+						<p class="p-morado">
+							Posiciona tu marca en la mente de tus clientes con un delicioso regalo.
+						</p><br>
+						<a href="#" class="a-morado">Chocolates Corporativos</a>
+					</div>
+				</div>
+				<div class="img-conte">
+					<img src="morado-3.jpg" class="img-morado-m"><br>
+					<div class="pes-morado">
+						<p class="p-morado">
+							Esta es tu oportunidad para ser un gran empresario en un mercado emergente.
+						</p><br>
+						<a href="#" class="a-morado">Quieres ser distribuidor</a>
+					</div>
+				</div>
+			</div><br>
+			<div class="naveg">
+				<p class="reconoc">Conoce mas de <b class="b-p">nuestros productos Aquí:</b></p>
+			    <div class="div-a"><a href="#" class="a-p-morado">Productos</a></div>
+			</div>
+		</div>
+		<div class="container-fruits">
+			<div class="chocolate">
+				<h4 class="h4-fruits">Blog de ideas <span class="m-s">dulces con chocolate</span></h4>
+				<p class="h4-fruits-p">
+					<b>Nuestro Blog está lleno de historias mágicas llenas de chocolate.</b> Tendencias de chocolatería, diseño, arte, ideas para regalos corporativos, historias de nuestros campesinos, casos de éxito y mucho más. Color Cacao <b>productora de chocolates finos artesanales.</b>
+				</p>
+			</div>
+			<div class="container-img-fruits">
+				<div class="conta-fruits">
+					<div class="conta-fruits-div">
+						<img src="debajomora-1.jpg" class="deba-mora">
+						<div class="div-img">
+							<h5 class="h5-img">Nuestros pensamientos sobre el artículo “De la flor del cacao a la taza de chocolate”</h5>
+							<p class="h5-p">
+								Imposible no sentirnos identificados con este artículo escrito por Fedecacao, definitivamente la flor del cacao es incomparable y es increíble como esa pequeña flor se convierte en chocolates de la más alt...
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="conta-fruits">
+					<div class="conta-fruits-div">
+						<img src="debajomora-2.jpg" class="deba-mora divs-2">
+						<div class="div-img div2">
+							<h5 class="h5-img">Recetas con chocolate: Cinnamon Rolls con Chocolate 65 % + Panela y Nibs de Cacao</h5>
+							<p class="h5-p">
+								Recetas con chocolate: Rollos de canela con CHOCOLATE! no vas a creer lo delicioso y saludable que pueden llegar a ser estos Cinnamon Rolls con Chocolate 65 % con Panela y Nibs de cacao Color Cacao una rec...
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="conta-fruits">
+					<div class="conta-fruits-div">
+						<img src="debajomora-3.jpg" class="deba-mora">
+						<div class="div-img">
+							<h5 class="h5-img">Receta con Chocolate: Pancakes de avena con trozos de chocolate</h5>
+							<p class="h5-p">
+								Hoy les traemos una nueva receta de chocolate que pueden preparar para sus desayunos, ideal para toda la familia y lo mejor, con un toque de chocolate perfecto para endulzar la mañana....
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="link-blog">
+				<a href="#" class="blog">Entra a nuestro Blog</a>
+		    </div>
+		</div>
+	</main>
+	<aside>
+		<div class="bottom-1">
+			<div class="b-1">
+				<div class="bottom-div-1">
+					<img src="logo_cacao.svg" class="logo-bottom">
+					<p class="bottom-p-1">
+						Somos una empresa dedicada a la elaboración de productos de chocolate, entre ellos bombones y tabletas. Somos cómplices de tus emociones; combinamos las técnicas artesanales de la Chocolatería Europea, con selectos cacaos finos de los campesinos colombianos para ofrecer experiencias únicas en sabor, aroma y textura.
+					</p>
+				</div>
+				<div class="bottom-from">
+					<p class="bottom-p-2">
+						Si quiere recibir noticias de Color Cacao registre su correo aquí:<br>
+						correo*
+					</p>
+					<form>
+						<input type="email" name="email" placeholder="Correo" class="celda"><br>
+						<input type="submit" value="Enviar" class="boton-enviar">
+					</form>
+					<p class="bottom-p-3">
+						Cree su propio <a href="#" class="HubSpot">formulario libre con HubSpot</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="bottom-1">
+			<div class="b-2">
+				<div class="contacto-div1">
+					<div class="medium">
+						<div class="p-1">
+							<h3 class="bottom-h3">Contáctanos</h3>
+							<p class="bottom-p-2">
+								Para mayor información te invitamos a entrar a la pagina de <a href="#" class="contac-aside">contacto</a>
+							</p>
+						</div>
+						<div class="p-1">
+							<p class="bottom-p-2">
+								Carrera 65 B No. 30-90 Medellín – Colombia
+							</p>
+						</div>
+						<div class="p-1">
+							<p class="bottom-p-2">
+								Cel/whatsapp: +57 (318) 254-4007 • +57 (313) 552-3620 • +57 (315) 812-6634
+							</p>
+						</div>
+						<div class="p-1">
+							<p class="bottom-p-2">
+								ventas@colorcacao.com comercial@colorcacao.com
+							</p>
+						</div>
+						<div class="p-12">
+							<ul class="p-12-ul">
+								<li class="li-12"><a href="#" class="li-13">Facebook</a></li>
+								<li class="li-12"><a href="#" class="li-13">Instagram</a></li>
+								<li class="li-12"><a href="#" class="li-13">Youtube</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="contacto-div1">
+					<div class="b-3">
+						<h3 class="bottom-h3">Productos</h3>
+						<img src="frijoles.jpg" class="img-productos">
+						<div class="productos-img">
+							<p class="bottom-p-2-1">
+								Bolitas de guayaba recubiertas de chocolate- 100g $12,000
+							</p>
+						</div>
+						<a href="#" class="tienda-as">ENTRA A LA TIENDA</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</aside>
+	<footer>
+		<div class="copyr">
+			<p class="footer-p">
+				Copyrights  &copy; 2020 All Rights Reserved by Color Cacao | Preguntas Frecuentes
+			</p>
+		</div>
+		<div class="diseño">
+			<p class="footer-p">
+				Diseñado y desarrollado por -Yeison- Estructurando MDV
+			</p>
+		</div>
+	</footer>
+</body>
+</html>
